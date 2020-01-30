317b4e36564fe690a17065278465cd4671b353a1
Bolbi94 <bolbi994@gmail.com>
2020-01-30 12:55:56 +0200

    creation

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './app.css';
+
+export default class App extends React.Component {
+  constructor(props) {
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
